af8b63195d543009a7eb3324d0c78ed4ec234ae3
pukar <pukars57@gmail..com>
2021-01-13 15:25:31 +0545

    Showed web information in aboutUs tab

--- app/src/main/java/com/pukar/softuser/fragments/AboutUsFragment.kt
+++ app/src/main/java/com/pukar/softuser/fragments/AboutUsFragment.kt
@@ -5,22 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import com.pukar.softuser.R
 
 class AboutUsFragment : Fragment() {
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
+    private lateinit var wvAboutUs : WebView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.fragment_about_us, container, false)
+        wvAboutUs=view.findViewById(R.id.wvAboutUs)
+        wvAboutUs.settings.javaScriptEnabled=true
+        val webViewClient = WebViewClient()
+        wvAboutUs.webViewClient=webViewClient
+        wvAboutUs.loadUrl("https://softwarica.edu.np")
         return view
     }
 
